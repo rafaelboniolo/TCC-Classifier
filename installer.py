84a9e9466688fb292d2676e7794284040ea8c31b
boniolo <33812322+rafaelboniolo@users.noreply.github.com>
2019-12-01 10:00:18 -0300

    remoção de scripts inuteis

--- installer.py
+++
@@ -1,4 +0,0 @@
-import os
-os.system(" echo Inicializando verificadores de dependencia ... ")
-os.system(" color 02, pip install numpy ")
-
--- src/deprecated/mode.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-from configdataset import ConfigDataset
-
-class Mode(Enum):
-    TRAIN = "C:\\Users\\rafae\\Documents\\GitHub\\TCC-Dataset\\dataset"
-    TEST = "C:\\Users\\rafae\\Documents\\GitHub\\TCC-Dataset\\test"
-    VAL = ""
